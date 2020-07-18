bb55400f257a4813b984d17796e638ed5d0cc78e
LamFra <57345846+LamFra@users.noreply.github.com>
2020-07-18 11:40:53 +0200

    Add files via upload

--- ModelloFisico.sql
+++ ModelloFisico.sql
@@ -7,13 +7,14 @@ CREATE TABLE IF NOT EXISTS JFLCorporation.Software (
   CodiceS INT UNSIGNED NOT NULL AUTO_INCREMENT,
   NomeS VARCHAR(30) NOT NULL,
   DescrizioneVetrinaS VARCHAR(500) NOT NULL,
-  ImmagineVetrinaS VARCHAR(100) NOT NULL,
+  ImmagineVetrinaS VARCHAR(2048) NOT NULL,
   DataRilascioS date, 
   TitoloDescrizioneSinteticaS VARCHAR (150) NOT NULL,
   DescrizioneSinteticaS VARCHAR(500) NOT NULL,
-  ImmagineDescrizioneS VARCHAR(100) NOT NULL,
+  ImmagineDescrizioneS VARCHAR(2048) NOT NULL,
   SpecificheS VARCHAR (2048) NOT NULL,
   PrezzoS FLOAT NOT NULL,
+  Disponibile bool NOT NULL DEFAULT true,
   
   PRIMARY KEY (CodiceS)
 )
@@ -24,12 +25,13 @@ CREATE TABLE IF NOT EXISTS JFLCorporation.Corso (
   CodiceC INT UNSIGNED NOT NULL AUTO_INCREMENT,
   NomeC VARCHAR(500) NOT NULL,
   DescrizioneVetrinaC VARCHAR(500) NOT NULL,
-  ImmagineVetrinaC VARCHAR(100) NOT NULL,
+  ImmagineVetrinaC VARCHAR(2048) NOT NULL,
   TitoloDescrizioneSinteticaC VARCHAR (150) NOT NULL,
   DescrizioneSinteticaC VARCHAR(500) NOT NULL,
-  ImmagineDescrizioneC VARCHAR(100) NOT NULL,
+  ImmagineDescrizioneC VARCHAR(2048) NOT NULL,
   SpecificheC VARCHAR (2048) NOT NULL,
   NumEsamiC INT NOT NULL,
+  Disponibile bool NOT NULL DEFAULT true,
   
   PRIMARY KEY (CodiceC)
 )
@@ -37,15 +39,17 @@ ENGINE=InnoDB;
 
 /*Creazione tabella Cliente*/
 CREATE TABLE IF NOT EXISTS JFLCorporation.Cliente (
-  CodiceFiscaleCL CHAR(16) NOT NULL,
+  Tipo char(1) default "S", 
+  UsernameCL VARCHAR(20) NOT NULL,
   NomeCL VARCHAR(50) NOT NULL,
   CognomeCL VARCHAR(50) NOT NULL,
   Data_nascitaCL DATE NOT NULL,
   IndirizzoCL VARCHAR(100) NOT NULL,
   EmailCL VARCHAR(50) NOT NULL,
-  PasswordCL VARCHAR(50) NOT NULL,
+  PasswordCL CHAR(32) NOT NULL,
   
-  PRIMARY KEY (CodiceFiscaleCL)
+  CHECK(UsernameCL != "null" AND EmailCL != "null"),
+  PRIMARY KEY (EmailCL)
 )
 ENGINE = InnoDB;
 
@@ -53,14 +57,42 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS JFLCorporation.Pagamento (
 	NumCarta CHAR(16) NOT NULL,
     DataScadenza  CHAR(5) NOT NULL,
-	Cliente_CodiceFiscaleCL CHAR(16) NOT NULL,
-	CVC CHAR(3) NOT NULL,
-    
-	CHECK(DataScadenza='__/__'),
-	PRIMARY KEY (NumCarta, Cliente_CodiceFiscaleCL),
-    FOREIGN KEY (Cliente_CodiceFiscaleCL)
-    REFERENCES JFLCorporation.Cliente (CodiceFiscaleCL)
+	Cliente_EmailCL VARCHAR(50) NOT NULL,
+	CVC CHAR(32) NOT NULL,
+
+	PRIMARY KEY (NumCarta, Cliente_EmailCL),
+    FOREIGN KEY (Cliente_EmailCL)
+    REFERENCES JFLCorporation.Cliente (EmailCL)
     
     ON DELETE CASCADE
+)
+ENGINE = InnoDB;
+
+/*Creazione tabella Acquista*/
+CREATE TABLE IF NOT EXISTS JFLCorporation.Acquista(
+	Cliente_EmailCL VARCHAR(50) NOT NULL,
+	Software_CodiceS INT UNSIGNED NOT NULL,
+    Data_acquisto DATE NOT NULL,
+    
+    PRIMARY KEY(Cliente_EmailCL,Software_CodiceS),
+	FOREIGN KEY (Cliente_EmailCL)
+    REFERENCES JFLCorporation.Cliente (EmailCL),
+    FOREIGN KEY (Software_CodiceS)
+    REFERENCES JFLCorporation.Software (CodiceS)
+)
+ENGINE = InnoDB;
+
+/*Creazione tabella Segue*/
+CREATE TABLE IF NOT EXISTS JFLCorporation.Segue(
+	Cliente_EmailCL VARCHAR(50) NOT NULL,
+	Corso_CodiceC INT UNSIGNED NOT NULL,
+    Numero_esami_conseguiti INT UNSIGNED DEFAULT 0,
+    Data_iscrizione DATE NOT NULL,
+    
+    PRIMARY KEY(Cliente_EmailCL,Corso_CodiceC),
+	FOREIGN KEY (Cliente_EmailCL)
+    REFERENCES JFLCorporation.Cliente (EmailCL),
+    FOREIGN KEY (Corso_CodiceC)
+    REFERENCES JFLCorporation.Corso (CodiceC)
 )
 ENGINE = InnoDB;
--- Popolamento.sql
+++ Popolamento.sql
@@ -13,7 +13,7 @@ values (
 			"Il software pensato per la gestione di uno studio medico",
 			"Contiene tutte le funzionalità per gestire lo studio medico privato.In particolare il programma gestisce il calendario dei medici, la cartella clinica del paziente e la fatturazione, in modo semplice e intuitivo.", 
             "softwareImg/1software.jpeg",
-            "Il software da noi progettato contiene tutte le funzionalità per la gestione accurata di uno studio medico privato.In particolare il programma gestisce il calendario dei medici, la cartella clinica del paziente e la fatturazione, in modo semplice e intuitivo. Il calendario infatti ha l’interfaccia grafica in stile Outlook, e si può passare semplicemente dalla visione giornaliera a quella settimanale e mensile. Tutte le finestre possono essere stampate. Se si tratta di uno Studio con più medici, il programma permette di gestire e condividere più calendari.Per ciascun paziente, oltre ai dati anagrafici ed un riepilogo degli appuntamenti e degli interventi effettuati, è possibile allegare file in qualunque formato, come PDF, immagini e file di testo. Il programma è predisposto per l’utilizzo in una rete locale, ed è possibile anche limitare l’accesso degli utenti. Una segretaria, ad esempio, può avere accesso ai dati anagrafici e di fatturazione, ma non alla cartella clinica del paziente. Viceversa un medico può non dover visualizzare i dati di fatturazione.Inoltre, prevede anche la possibilità di gestire un magazzino di prodotti o farmaci, e la stampa automatica dei documenti utilizzati più di frequente, come l’approvazione della privacy e lo scarico di responsabilità, e prevede la generazione semplificata delle ricette su carta intestata. Il programma è totalmente personalizzabile e compatibile con tutti i sistemi Windows. Non perdere questa novità!",
+            "Il software da noi progettato contiene tutte le funzionalità per la gestione accurata di uno studio medico privato.In particolare il programma gestisce il calendario dei medici, la cartella clinica del paziente e la fatturazione, in modo semplice e intuitivo. Il calendario infatti ha l\'interfaccia grafica in stile Outlook, e si può passare semplicemente dalla visione giornaliera a quella settimanale e mensile. Tutte le finestre possono essere stampate. Se si tratta di uno Studio con più medici, il programma permette di gestire e condividere più calendari.Per ciascun paziente, oltre ai dati anagrafici ed un riepilogo degli appuntamenti e degli interventi effettuati, è possibile allegare file in qualunque formato, come PDF, immagini e file di testo. Il programma è predisposto per l\'utilizzo in una rete locale, ed è possibile anche limitare l\'accesso degli utenti. Una segretaria, ad esempio, può avere accesso ai dati anagrafici e di fatturazione, ma non alla cartella clinica del paziente. Viceversa un medico può non dover visualizzare i dati di fatturazione.Inoltre, prevede anche la possibilità di gestire un magazzino di prodotti o farmaci, e la stampa automatica dei documenti utilizzati più di frequente, come l\'approvazione della privacy e lo scarico di responsabilità, e prevede la generazione semplificata delle ricette su carta intestata. Il programma è totalmente personalizzabile e compatibile con tutti i sistemi Windows. Non perdere questa novità!",
             140
 		);
 insert into software (CodiceS,NomeS,DescrizioneVetrinaS, ImmagineVetrinaS, DataRilascioS, TitoloDescrizioneSinteticaS, DescrizioneSinteticaS, ImmagineDescrizioneS, SpecificheS, PrezzoS) 
@@ -26,10 +26,11 @@ values (
 			"Il software adatto a tutti i tipi di aziende",
 			"Contiene tutte le funzionalità per gestire le aziende. Prevede la possibilità di inserire e gestire le consegne, i dati relativi ai clienti e la gestione delle fatture.", 
             "softwareImg/2software.jpeg", 
-            "Il nostro applicativo contiene tutte le funzionalità per gestire le agenzie di pony express. Prevede la possibilità di inserire e gestire le consegne, i dati relativi ai clienti e la gestione delle fatture. Una ricca dotazione di stampe, statistiche e strumenti di controllo lo rendono uno strumento indispensabile per le agenzie di pony express. La versione qui presentata è adatta all'utilizzo su postazioni singole. Il programma è totalmente personalizzabile e compatibile con tutti i sistemi Windows.Non perdere quest'occasione!",
+            "Il nostro applicativo contiene tutte le funzionalità per gestire le agenzie di pony express. Prevede la possibilità di inserire e gestire le consegne, i dati relativi ai clienti e la gestione delle fatture. Una ricca dotazione di stampe, statistiche e strumenti di controllo lo rendono uno strumento indispensabile per le agenzie di pony express. La versione qui presentata è adatta all\'utilizzo su postazioni singole. Il programma è totalmente personalizzabile e compatibile con tutti i sistemi Windows.Non perdere quest\'occasione!",
             150
 		);
-insert into software (CodiceS,NomeS,DescrizioneVetrinaS, ImmagineVetrinaS, DataRilascioS, TitoloDescrizioneSinteticaS, DescrizioneSinteticaS, ImmagineDescrizioneS, SpecificheS, PrezzoS) values (
+insert into software (CodiceS,NomeS,DescrizioneVetrinaS, ImmagineVetrinaS, DataRilascioS, TitoloDescrizioneSinteticaS, DescrizioneSinteticaS, ImmagineDescrizioneS, SpecificheS, PrezzoS) 
+values (
 			3,
 			"InvoiceJFL", 
 			"Il software nato per facilitare e velocizzare la fatturazione",
@@ -38,7 +39,7 @@ insert into software (CodiceS,NomeS,DescrizioneVetrinaS, ImmagineVetrinaS, DataR
 			"Il software nato per semplificare la fatturazione",
 			"Contiene tutte le funzionalità per gestire le tue fatture.Con la nuova versione rinnovata, la veste grafica è ancora più intuitiva, versatile e gradevole.", 
             "softwareImg/3software.jpeg", 
-            "Il nostro programma che contiene tutte le funzionalità per gestire le tue fatture. Con la nuova versione rinnovata, la veste grafica è ancora più intuitiva, versatile e gradevole. ompatibile con l'aliquota IVA al 22%.Abbiamo affinato le tecniche per rendere l’emissione delle fatture, dei preventivi e delle note di credito un’azione pratica ed efficace.Il nostro applicativo offre strumenti di gestione e controllo avanzati. In pochi secondi saprai quante fatture hai emesso a favore di un tuo cliente, oppure potrai visualizzare e stampare tutte le fatture non pagate, o ancora stampare la lista dei tuoi clienti, con indirizzo e numero telefonico. La ricerca dei clienti o delle fatture richiede solo pochi secondi, e puoi sempre stampare i risultati delle ricerche.Non perdere quest'opportunità!",
+            "Il nostro programma che contiene tutte le funzionalità per gestire le tue fatture. Con la nuova versione rinnovata, la veste grafica è ancora più intuitiva, versatile e gradevole. ompatibile con l'aliquota IVA al 22%.Abbiamo affinato le tecniche per rendere l\'emissione delle fatture, dei preventivi e delle note di credito un\'azione pratica ed efficace.Il nostro applicativo offre strumenti di gestione e controllo avanzati. In pochi secondi saprai quante fatture hai emesso a favore di un tuo cliente, oppure potrai visualizzare e stampare tutte le fatture non pagate, o ancora stampare la lista dei tuoi clienti, con indirizzo e numero telefonico. La ricerca dei clienti o delle fatture richiede solo pochi secondi, e puoi sempre stampare i risultati delle ricerche.Non perdere quest\'opportunità!",
             160
 		);
 insert into software (CodiceS,NomeS,DescrizioneVetrinaS, ImmagineVetrinaS, DataRilascioS, TitoloDescrizioneSinteticaS, DescrizioneSinteticaS, ImmagineDescrizioneS, SpecificheS, PrezzoS) 
@@ -51,7 +52,7 @@ values (
 			"Il software progettato per facilitare la gestione di un negozio o di un magazzino di abbigliamento",
 			"Il nostro software contiene tutte le funzionalità per gestire con efficacia un negozio o un magazzino di abbigliamento.", 
             "softwareImg/4software.jpeg", 
-            "Il nostro programma che contiene tutte le funzionalità per gestire con efficacia un negozio o un magazzino di abbigliamento. Esso ffre strumenti di gestione e controllo avanzati. In pochi secondi potrai visualizzare e stampare l’esistenza nel magazzino, o stampare la lista della merce scaricata (venduta) per data, per fornitore, per articolo, o una qualunque combinazione, o ancora quanto hai acquistato da un fornitore, o quante fatture hai emesso a favore di un tuo cliente. Guarda i video: vedrai come funzionano il carico e lo scarico, e gli strumenti di ricerca.Anche la gestione del magazzino è studiata per la massima efficacia.Non perdere questa novità!",
+            "Il nostro programma che contiene tutte le funzionalità per gestire con efficacia un negozio o un magazzino di abbigliamento. Esso ffre strumenti di gestione e controllo avanzati. In pochi secondi potrai visualizzare e stampare l\'esistenza nel magazzino, o stampare la lista della merce scaricata (venduta) per data, per fornitore, per articolo, o una qualunque combinazione, o ancora quanto hai acquistato da un fornitore, o quante fatture hai emesso a favore di un tuo cliente. Guarda i video: vedrai come funzionano il carico e lo scarico, e gli strumenti di ricerca.Anche la gestione del magazzino è studiata per la massima efficacia.Non perdere questa novità!",
             160
 		);
 insert into software (CodiceS,NomeS,DescrizioneVetrinaS, ImmagineVetrinaS, DataRilascioS, TitoloDescrizioneSinteticaS, DescrizioneSinteticaS, ImmagineDescrizioneS, SpecificheS, PrezzoS) 
@@ -64,7 +65,7 @@ values (
 			"Il software creato per semplificare le vostre azioni",
 			"Il nostro applicativo grazie ad un sistema evoluto di gestione dati facilita l'esperienza paziente/specialista.", 
             "softwareImg/5software.jpeg", 
-            "Un nuovo modo per seguire un programma alimentare o una fase di riabilitazione. L'assistente virtuale permette grazie ad un sistema evoluto di gestione dati di seguire programmi personalizzati attraverso l'utilissimo sistema delle notifiche garantendo un'esperienza paziente/specialista piu' consapevole con il supporto della tecnologia.La videochiamata live consente ai professionisti del settore di fornire un servizio aggiuntivo ai propri pazienti dando la possibilita'di trasferire informazioni di natura tecnica.",
+            "Un nuovo modo per seguire un programma alimentare o una fase di riabilitazione. L\'assistente virtuale permette grazie ad un sistema evoluto di gestione dati di seguire programmi personalizzati attraverso l\'utilissimo sistema delle notifiche garantendo un\'esperienza paziente/specialista piu' consapevole con il supporto della tecnologia.La videochiamata live consente ai professionisti del settore di fornire un servizio aggiuntivo ai propri pazienti dando la possibilita'di trasferire informazioni di natura tecnica.",
             170
 		);
         
@@ -111,7 +112,7 @@ values
             "Corso Java Spring & Hibernate (con JPA)",
             "Il corso parte da zero ma è necessario avere una discreta conoscenza di HTML5 e CSS3, oltre che una conoscenza base di Javascript.",
 			"corsoImg/3corso.jpeg",
-			"Il corso parte da zero ma è necessario avere una discreta conoscenza di HTML5 e CSS3, oltre che una conoscenza base di Javascript. Non è necessaria la conoscenza di Angular JS o di Angular 2 né di JQuery. E’ consigliata la conoscenza di Java SE, sebbene possa essere sufficiente l’esperienza in un qualsiasi linguaggio di programmazione affermato (piattaforma .NET, oppure C/C++ o PHP).",
+			"Il corso parte da zero ma è necessario avere una discreta conoscenza di HTML5 e CSS3, oltre che una conoscenza base di Javascript. Non è necessaria la conoscenza di Angular JS o di Angular 2 né di JQuery. E\' consigliata la conoscenza di Java SE, sebbene possa essere sufficiente l\'esperienza in un qualsiasi linguaggio di programmazione affermato (piattaforma .NET, oppure C/C++ o PHP).",
 			6
 );
 insert into corso (CodiceC,NomeC,DescrizioneVetrinaC, ImmagineVetrinaC, TitoloDescrizioneSinteticaC, DescrizioneSinteticaC, ImmagineDescrizioneC, SpecificheC, NUmEsamiC) 
@@ -127,5 +128,3 @@ values
 			"Il corso Blockhain in Java parte da zero ma è necessario avere una discreta conoscenza della programmazione in un generico linguaggio.Il corso Blockchain in Java non richiede particolari competenze matematiche per essere seguito con profitto e presenta gli argomenti in maniera rigorosa ma facilmente comprensibile.Il corso Blockchain in Java nasce per essere erogato direttamente presso la struttura del Cliente ma può avere anche edizioni in aula programmate a calendario",
 			8
 );
-
-
